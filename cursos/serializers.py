f25afecad91b78f3124379c435b68d2364acd52c
vinicius <car_silva@hotmail.com>
2021-09-28 21:43:16 -0300

    instacao do drf e configuracao de rotas/views/models

---
+++ cursos/serializers.py
@@ -0,0 +1,38 @@
+from rest_framework import serializers
+from .models import Curso, Avaliacao
+
+
+class AvaliacaoSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        extra_kwargs = {
+            'email': {'write_only': True}  # o email nao vai ser apresentado qnd alguem consultar
+        }
+        model = Avaliacao
+        #  campos para mostrar pro usuario
+        fields = (
+            'id',
+            'curso',
+            'nome',
+            'email',
+            'comentario',
+            'avaliacao',
+            'criacao',
+            'ativo'
+        )
+
+
+class CursoSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Curso
+        fields = (
+            'id',
+            'titulo',
+            'url',
+            'criacao',
+            'ativo'
+        )
+
+
+# string binaria é bem mais rapido que uma string normal, ela comeca com b'
---
+++ cursos/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+
+from .views import CursoAPIView, AvaliacaoAPIView
+
+urlpatterns = [
+    # endpoints
+    path('cursos/', CursoAPIView.as_view(), name='cursos'),
+    path('avaliacoes/', AvaliacaoAPIView.as_view(), name='avaliacoes'),
+]
